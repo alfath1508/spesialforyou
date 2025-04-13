40928b1a10ee22e641489a42d12590334f11709f
alfath1508 <alfath.fathurrahman1508@gmail.com>
2025-04-13 22:45:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function sobekTiket() {
     const ticket = document.querySelector('.ticket');
     ticket.classList.add('torn');
     setTimeout(() => {
-        ticket.innerHTML = "<p style='margin:auto;'>Yeay, ditunggu jawaban kamu ya!</p>";
+        ticket.innerHTML = "<p style='margin:auto;'>Yeayyyyy, Saya ingin mengajak kamu untuk kita nonton bareng, jadi kapan nih? (btw chat ku dibalas yaaa)</p>";
         ticket.classList.remove('torn');
     }, 700);
 }
